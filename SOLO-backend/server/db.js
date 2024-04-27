c1eea8d19be045cfc8ad46bdc912dc92e0229dd0
Farahnaz Hoque <107436970+farahnazhoque@users.noreply.github.com>
2024-04-27 14:02:55 -0700

    route works

--- SOLO-backend/server/db.js
+++ SOLO-backend/server/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: "solotestdb.cxqsaw0a4eyq.us-west-1.rds.amazonaws.com",
   user: "admin",
-  port: 3000,  // Specify the port if not the default
+  port: 3306,  // Specify the port if not the default
   password: "SoloTestDB",
   database: "SoloTestDB"
 });

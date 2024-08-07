b39484ef985178a8a2259c35f2cbe06de22e3e04
Farahnaz Hoque <107436970+farahnazhoque@users.noreply.github.com>
2024-08-08 05:55:57 +0600

    asssign workout frontend done

--- SOLO-frontend/src/pages/CoachView/AthleteProfile.tsx
+++ SOLO-frontend/src/pages/CoachView/AthleteProfile.tsx
@@ -1,6 +1,23 @@
 import React, { useState, useEffect, useContext, ChangeEvent } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
-import { IonPage, IonHeader, IonToolbar, IonContent, IonButtons, IonBackButton, IonCardTitle, IonCardSubtitle, IonButton, IonModal } from '@ionic/react';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonContent,
+  IonButtons,
+  IonBackButton,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonButton,
+  IonModal,
+  IonInput,
+  IonItem,
+  IonSelect,
+  IonSelectOption,
+  IonLabel,
+  IonDatetime,
+} from '@ionic/react';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import '../../components/CoachView/ProfileView.css';
 import TabBar from './TabBar';
@@ -25,6 +42,18 @@ interface MediaItem {
   signedUrl: string;
 }
 
+interface Workout {
+  name: string;
+  workoutType: string;
+}
+
+interface Task {
+  title: string;
+  intensity: string;
+  time: number;
+  status: 'Incomplete' | 'Complete';
+}
+
 const CurrentAthleteView: React.FC = () => {
   const history = useHistory();
   const location = useLocation();
@@ -34,8 +63,14 @@ const CurrentAthleteView: React.FC = () => {
 
   const [currentAthlete, setCurrentAthlete] = useState<AthleteInfo | null>(null);
   const [currentMedia, setCurrentMedia] = useState<MediaItem[]>([]);
-  const [showModal, setShowModal] = useState(false);
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [showMediaModal, setShowMediaModal] = useState(false);
+  const [showTaskModal, setShowTaskModal] = useState(false);
   const [validationMessage, setValidationMessage] = useState<string>("");
+  const [selectedWorkoutType, setSelectedWorkoutType] = useState<string | null>(null);
+  const [workouts, setWorkouts] = useState<Workout[]>([]);
+  const [selectedWorkout, setSelectedWorkout] = useState<string | null>(null);
+  const [dueDate, setDueDate] = useState<string>("");
 
   useEffect(() => {
     const fetchAthlete = async () => {
@@ -64,6 +99,21 @@ const CurrentAthleteView: React.FC = () => {
     fetchMedia();
   }, [athleteId, coachId]);
 
+  useEffect(() => {
+    if (selectedWorkoutType) {
+      fetchWorkoutsByType(selectedWorkoutType);
+    }
+  }, [selectedWorkoutType, athleteId]);
+
+  const fetchWorkoutsByType = async (workoutType: string) => {
+    try {
+      const workouts = await ApiService.getWorkoutsByUserAndType(coachId, workoutType);
+      setWorkouts(workouts);
+    } catch (error) {
+      console.error(`Failed to fetch ${workoutType} workouts:`, error);
+    }
+  };
+
   const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
     const fileList = event.target.files;
     if (fileList && fileList[0]) {
@@ -83,7 +133,7 @@ const CurrentAthleteView: React.FC = () => {
 
       await storeMediaInTable(file, fileName, athleteId);
 
-      setShowModal(false);
+      setShowMediaModal(false);
       setValidationMessage("");
     }
   };
@@ -117,7 +167,7 @@ const CurrentAthleteView: React.FC = () => {
     } catch (error) {
       console.error('Error accessing camera:', error);
     }
-    setShowModal(false);
+    setShowMediaModal(false);
   };
 
   const storeMediaInTable = async (file: File, title: string, athleteId: string | null) => {
@@ -155,6 +205,39 @@ const CurrentAthleteView: React.FC = () => {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+  };
+
+  const handleAssignWorkout = async () => {
+    if (!selectedWorkoutType || !selectedWorkout || !dueDate) {
+      setValidationMessage("Workout type, workout, and due date are required.");
+      return;
+    }
+
+    // Add workout assignment logic here
+    console.log('Assigned workout:', selectedWorkoutType, selectedWorkout, dueDate);
+
+    setTasks([...tasks, {
+      title: selectedWorkout!,
+      intensity: selectedWorkoutType!,
+      time: new Date(dueDate).getTime(), // example for time, it should be actual workout time
+      status: 'Incomplete'
+    }]);
+
+    setShowTaskModal(false);
+    setSelectedWorkoutType(null);
+    setSelectedWorkout(null);
+    setDueDate("");
+    setValidationMessage("");
+  };
+
+  const toggleTaskStatus = (index: number) => {
+    const updatedTasks = tasks.map((task, i) => {
+      if (i === index) {
+        return { ...task, status: task.status === 'Incomplete' ? 'Complete' : 'Incomplete' };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
   };
 
   if (!currentAthlete) {
@@ -199,8 +282,28 @@ const CurrentAthleteView: React.FC = () => {
           onViewMore={() => console.log('View more current media')}
           onDownload={downloadMedia} // Pass the download function
         />
-        <IonModal isOpen={showModal} onDidDismiss={() => setShowModal(false)}>
-          <div className="white-modal" style={{ padding: '20px' }}>
+        <div style={{ padding: '10px 20px' }}>
+        <h1>Tasks</h1>
+
+          <IonCardTitle style={{ fontSize: '24px', fontWeight: 'bold' }}>Task</IonCardTitle>
+          {tasks.map((task, index) => (
+            <div key={index} className="task-item">
+              <div className="task-info">
+                <p>Title: {task.title}</p>
+                <p>Intensity Level: {task.intensity}</p>
+                <p>Time: {task.time} mins</p>
+              </div>
+              <button
+                className={`complete-button ${task.status === 'Complete' ? 'complete' : ''}`}
+                onClick={() => toggleTaskStatus(index)}
+              >
+                {task.status}
+              </button>
+            </div>
+          ))}
+        </div>
+        <IonModal isOpen={showMediaModal} onDidDismiss={() => setShowMediaModal(false)} className="fullscreen-modal">
+          <div className="modal-content">
             <h1 className="modal-title">Media Preference</h1>
             <IonButton expand="block" className="my-custom-button" onClick={() => document.getElementById('file-upload')?.click()}>
               Upload Media
@@ -208,7 +311,7 @@ const CurrentAthleteView: React.FC = () => {
             <IonButton expand="block" className="my-custom-button" onClick={takePhoto}>
               Take Photo
             </IonButton>
-            <IonButton expand="block" className="cancel-button" onClick={() => setShowModal(false)}>
+            <IonButton expand="block" className="cancel-button" onClick={() => setShowMediaModal(false)}>
               Cancel
             </IonButton>
           </div>
@@ -220,14 +323,73 @@ const CurrentAthleteView: React.FC = () => {
             onChange={handleFileChange}
           />
         </IonModal>
-        {validationMessage && <div className="error-message">{validationMessage}</div>}
+        <IonModal isOpen={showTaskModal} onDidDismiss={() => setShowTaskModal(false)} className="fullscreen-modal">
+          <div className="modal-content">
+            <h1 className="modal-title">Assign Workout</h1>
+            <IonItem>
+              <IonLabel>Workout Type</IonLabel>
+              <IonSelect
+                value={selectedWorkoutType}
+                placeholder="Select workout type"
+                onIonChange={(e) => setSelectedWorkoutType(e.detail.value)}
+              >
+                <IonSelectOption value="standard">Standard</IonSelectOption>
+                <IonSelectOption value="dynamic">Dynamic</IonSelectOption>
+                <IonSelectOption value="competition">Competition</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            {selectedWorkoutType && (
+              <IonItem>
+                <IonLabel>Workout</IonLabel>
+                <IonSelect
+                  value={selectedWorkout}
+                  placeholder="Select workout"
+                  onIonChange={(e) => setSelectedWorkout(e.detail.value)}
+                >
+                  {workouts.map((workout, index) => (
+                    <IonSelectOption key={index} value={workout.name}>
+                      {workout.name}
+                    </IonSelectOption>
+                  ))}
+                </IonSelect>
+              </IonItem>
+            )}
+            {selectedWorkoutType && (
+              <IonItem>
+                <IonLabel>Due Date</IonLabel>
+                <IonDatetime
+                  displayFormat="MMM DD, YYYY"
+                  min="2023-01-01"
+                  max="2030-12-31"
+                  value={dueDate}
+                  onIonChange={e => setDueDate(e.detail.value!)}
+                  presentation="date"
+                />
+              </IonItem>
+            )}
+            {validationMessage && <div className="error-message">{validationMessage}</div>}
+            <IonButton expand="block" className="my-custom-button" onClick={handleAssignWorkout}>
+              Assign workout
+            </IonButton>
+            <IonButton expand="block" className="cancel-button" onClick={() => setShowTaskModal(false)}>
+              Cancel
+            </IonButton>
+          </div>
+        </IonModal>
       </IonContent>
       <IonButton
         className="add-media-button"
-        style={{ position: 'fixed', bottom: '66px', right: '16px', zIndex: 1000 }}
-        onClick={() => setShowModal(true)}
+        style={{ position: 'fixed', bottom: '135px', right: '16px', zIndex: 1000 }}
+        onClick={() => setShowTaskModal(true)}
       >
-        + Add Media
+        + Assign workout
+      </IonButton>
+      <IonButton
+        className="add-media-button"
+        style={{ position: 'fixed', bottom: '66px', right: '16px', zIndex: 1000 }}
+        onClick={() => setShowMediaModal(true)}
+      >
+        + Add media
       </IonButton>
       <TabBar />
     </IonPage>
